0d9b0b5806a998302514f1d8f976f41ce21425e1
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-03-07 21:34:35 +0530

    endpiont data preprocessing

---
+++ utils/PreProcessing.py
@@ -0,0 +1,61 @@
+import requests
+import os
+import logging
+
+
+# Ensure the "logs" directory exists
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+# logging configuration
+logger = logging.getLogger('PreProcessing')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir, 'PreProcessing.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+
+class preprocessing:
+    def get_json(self, api:str, data_endpoint:str):
+        """
+        Returns the json object for the provided endpoint
+        Args:
+        api : api key for the end point
+        data_endpoint: endpoint to fetch data from 
+
+        Returns:
+        json object of the retrived data
+        """
+        logger.log(logging.INFO, "Started json file retriver ")
+        # Define the headers with API Key
+        headers = {
+            "Content-Type": "application/json",
+            "Flic-Token": api
+        }
+        logger.log(logging.INFO, "Created the required header file")
+
+
+        # Send GET request
+        logger.log(logging.INFO, "Successfully Hit the endpoint")
+        response = requests.get(data_endpoint, headers=headers)
+        logger.log(logging.INFO, "Successfully got response from endpoint")
+
+        # Check if request was successful
+        if response.status_code == 200:
+            print("Response Successfull:")  
+        else:
+            logger.error(logging.ERROR,f"Error: {response.status_code}, Message: {response.text}")  # Print error details
+        return response.json()
+
